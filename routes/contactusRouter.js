8d02798b7bcf094b55cb92260afd472740c55ced
CruzChua2001 <2001cruzchua@gmail.com>
2026-03-01 23:20:32 +0800

    feat (add): Added a contactus page

--- routes/contactusRouter.js
+++ routes/contactusRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const REPORTTYPES = ["General Inquiry", "Feedback", "Report a Problem"];
+
 router.get("/", (req, res) => {
-    res.send("testing")
+    res.render("contactus/contactus", {
+        submitted: false,
+        reportTypes: REPORTTYPES
+    });
 })
 
 module.exports = router;  
