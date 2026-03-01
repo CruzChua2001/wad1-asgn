2567741ed3807cf6d8b7d6e63620bea5e7c0a281
CruzChua2001 <2001cruzchua@gmail.com>
2026-03-01 17:38:31 +0800

    feat (add): Added routing for main features

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const server = express();
+const path = require("path");
 
 const HOSTNAME = "localhost";
 const PORT = 8000;
 
 server.set("view engine", "ejs");
 server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, "public")))
 
 server.get("/health", (req, res) => {
     res.send("Server is running");
 })
 
-
 // Insert code below -------------------------
 
+const userRouter = require("./routes/userRouter")
+const eventRouter = require("./routes/eventRouter");
+const reserveRouter = require("./routes/reserveRouter");
+const configurationRouter = require("./routes/configurationRouter");
+const feedbackRouter = require("./routes/feedbackRouter");
 
+server.use("/user", userRouter);                        // Anumitaa 
+server.use("/event", eventRouter)                       // Claudine
+server.use("/reserve", reserveRouter);                  // Zhi Yang
+server.use("/configuration", configurationRouter);      // Mahshuk
+server.use("/feedback", feedbackRouter);                // Keifer
 
 // Insert code above -------------------------
 
