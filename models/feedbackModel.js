bbe682d551800b16d815e3e81a01dc06d2cfb2d2
keiferloke2025-collab <keifer.loke.2025@smu.edu.sg>
2026-03-22 15:50:12 +0800

    new feedback

--- models/feedbackModel.js
+++ models/feedbackModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { generateUUID } = require('../utils/uuidUtils');
+const {formatDateTime} = require('../utils/dateUtils')
 
 const feedbackSchema = new mongoose.Schema({
     FeedbackID: {
@@ -30,10 +32,55 @@ const feedbackSchema = new mongoose.Schema({
         type: Number,
         default: 0,
         enum: [0, 1]
-    }
+    },
+    rating: { type: Number, default: 0 },
+    attend: { type: Number, default: 0 },
+    recommend: {type: Number, default:0},
+    goals: {type:Number, default:0},
+    EventName : {type: String, 
+                required: [true, 'Feedback require a eventName']}
 })
 
 const Feedback = mongoose.model('Feedback', feedbackSchema, 'feedback');
+
+exports.addfeedback = async(userId, eventId, eventName, body) => {
+    return Feedback.create(
+    {FeedbackID: generateUUID(),
+    EventID: eventId,
+    UserID: userId,
+    Feedback: body.feedback || "",
+    CreatedDateTime: formatDateTime(new Date().toISOString()),
+    isAnonymous: Number(body.isAnonymous),
+    isDeleted: 0,
+    rating: Number(body.rating),
+    attend: Number(body.attend),
+    recommend: Number(body.recommend),
+    goals:Number(body.goals),
+    EventName: eventName
+    },
+)
+}
+exports.getTopEvents =  () => {
+    return Feedback.aggregate([
+      {
+        $addFields: {
+          totalScore: { $add: ["$rating", "$attend", "$recommend", "$goals"] }
+        }
+      },
+      {
+        $group: {
+          _id: "$EventId",
+          EventName: { $first: "$EventName" },
+          avgScore: { $avg: "$totalScore" }, 
+          numResponses: { $sum: 1 }          
+        }
+      },
+
+      { $sort: { avgScore: -1 } },
+
+      { $limit: 10 }
+    ]);
+}
 
 exports.retrieveFeedbackByEventID = (eventID) => {
     return Feedback.find({ EventID: eventID, isDeleted: 0 });
--- routes/feedbackRouter.js
+++ routes/feedbackRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-
+const feedbackController = require("../controllers/feedbackController");
+router.use(requireAuth)
 router.get("/", (req, res) => {
     res.send("testing")
 })
+router.get("/:eventId", feedbackController.getFeedbackForm);
+router.post("/:eventId", requireAuth,feedbackController.submitFeedback)
 
 module.exports = router;  
+
+
