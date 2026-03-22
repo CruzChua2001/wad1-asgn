e91bc371912e7b8eaf508810312cf87a6b952468
mahshukurra2025 <264659135+mahshukCodes@users.noreply.github.com>
2026-03-22 18:55:16 +0800

    Updated basic CRUD functions for Category Schema

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { formatDateTime } = require('../utils/dateUtils');
 
 const categorySchema = new mongoose.Schema({
     CategoryID: {
@@ -9,11 +10,15 @@ const categorySchema = new mongoose.Schema({
         type: String,
         required: [true, "Category require a CategoryName"]
     },
+    CategoryDesc:{
+        type:String,
+        required:[true,"Category require a valid Description"]
+    },
     Approval: {
         type: String,
         required: [true, "Category require an Approval"]
     },
-    CreatedBy: {
+    createdBy: {
         type: String,
         required: [true, "Category require a CreatedBy"]
     },
@@ -24,11 +29,28 @@ const categorySchema = new mongoose.Schema({
     isDeleted: {
         type: Number,
         default: 0
+    },
+    createdAt:{
+        type:Date,
+        required:[true,"Category require a Creation Date and Time Recorded"]
     }
 })
 
 const Category = mongoose.model("Category", categorySchema, "category");
 
+
+
 exports.retrieveAll = () => {
     return Category.find({ isDeleted: 0 });
+}
+
+exports.addCategory = (newCategory) =>{
+    return Category.create(newCategory)
+}
+exports.updateDetail = (id,name,desc) =>{
+    return Category.updateOne({CategoryID:id},{CategoryName:name,CategoryDesc:desc})
+}
+
+exports.deleteCategory = (id) =>{
+    return Category.updateOne({CategoryID:id},{isDeleted:1})
 }
--- routes/configurationRouter.js
+++ routes/configurationRouter.js
@@ -1,8 +1,257 @@
 const express = require("express");
 const router = express.Router();
+const uuidUtil = require("../utils/uuidUtils.js")
+const dateUtil = require("../utils/dateUtils.js")
 
+const userModel = require("../models/userModel.js");
+const categoryModel = require("../models/categoryModel.js")
+
+//DUMMY DATA 
+
+
+
+
+let reservationsToApprove = [1,4,7]
+
+let reservationHistory = [5,7,8]
+//BASIC ADMIN INTERFACE
 router.get("/", (req, res) => {
-    res.send("testing")
+   
+    res.render("configuration/configuration.ejs")
 })
 
 module.exports = router;  
+
+
+//CATEGORY ROUTES
+router.get("/category",async(req,res)=>{
+  const categoryList = await getAllCategories();
+  res.render("configuration/category.ejs",{categoryList,isRegister:false,validEntry:true,adminNameList:null,errorList:null,record:null})
+
+})
+
+router.get("/category/register",async(req,res)=>{
+    let adminNameList = await getAllAdmins()
+    let categoryList = await getAllCategories();
+
+    res.render("configuration/category.ejs",{categoryList,isRegister:true,validEntry:true,adminNameList,errorList:null,record:null})
+});
+
+router.post("/category/register",async(req,res)=>{
+  let categoryName = req.body.categoryName.trim();
+  let categoryDescription = req.body.categoryDescription.trim();
+  let approver = req.body.approver;
+  let adminNameList = await getAllAdmins()
+  let categoryList = await getAllCategories();
+  let validity = validateCategoryEntry(categoryDescription,categoryName,approver,categoryList);
+  let result;
+  let isRegister = true;
+  if(validity.validEntry){
+    try {
+      result = await categoryModel.addCategory(
+      {
+    CategoryID:uuidUtil.generateUUID(),
+    CategoryName:categoryName,
+    CategoryDesc:categoryDescription,
+    Approval:approver,
+    createdBy:req.user.userId,
+    RejectionReason:" ",
+    isDeleted:0,
+    createdAt:dateUtil.formatDateTime(new Date())
+    });
+    isRegister=false;
+    res.redirect("/configuration/category")
+    } catch (error) {
+      result = "fail";
+      console.error("Error adding new category",error);
+    }
+ 
+  }else{
+    res.render("configuration/category.ejs",{categoryList,isRegister,validEntry:validity.validEntry,adminNameList,errorList:validity.errorList,record:{categoryName,categoryDescription,approver}})
+
+  }
+});
+
+
+router.get("/categoryDetail", async(req,res)=>{
+  const categoryId = req.query.categoryId;
+  let catDetail;
+  try {
+    catDetail = await getCategoryById(categoryId);
+    catDetail.createdAt = dateUtil.formatDateTime(String(catDetail.createdAt))
+
+  } catch (error) {
+    console.error("Error retrieving Category Detail",error)
+  }
+  
+  let categoryData = {
+
+    catDetail,
+    events:[
+      // "event A","event B", "event C"
+    ]
+  }
+  res.render("configuration/categoryDetail.ejs",{categoryData,errorList:null,message:null})
+});
+
+router.post("/categoryDetail",async(req,res)=>{
+  const id = req.body.categoryId;
+  const newName = req.body.catNameUpdated;
+  const newDesc = req.body.catDescUpdated;
+  
+  let result,message;
+  if(newName && newDesc){
+    try {
+      result = await categoryModel.updateDetail(id,newName,newDesc)
+    } catch (error) {
+      console.log("Error updating Category Detail",error)
+    }
+    message = result? `Details for Category Id <b>${id}</b>, are updated successfully.`:`Error updating Category Id <b>${id}</b>, please try again later.`;
+    res.render("configuration/outcome.ejs",{
+      message,result
+    })
+
+  }else{
+    let errorList = [];
+    !newDesc ? errorList.push("Missing Category Description"):newDesc;
+    !newName ? errorList.push("Missing Category Name"):newName;
+    let categoryData = {
+
+    
+      catDetail:{
+        CategoryID:id,
+        createdBy:req.body.creator,
+        createdAt:req.body.categoryDate,
+        Approval:req.body.approver,
+        CategoryName:newName,
+        CategoryDesc:newDesc
+      },
+      
+      events:[
+        // "event A","event B", "event C"
+      ]
+    }
+
+    res.render("configuration/categoryDetail.ejs",{categoryData,message,errorList,})
+  }
+  
+});
+
+router.get("/deleteCategory",async(req,res) => {
+  const id = req.query.categoryId;
+  const option = (req.query.option).trim().toLowerCase();
+  let result;
+  if (option==="delete"){
+    try {
+      result = await categoryModel.deleteCategory(id)
+      
+      message = result? `Category Id <b>${id}</b> is deleted successfully.`:`Error deleting Category Id <b>${id}</b>, please try again later.`;
+
+    } catch (error) {
+        console.error(`Error deleting Category Id ${id}`,error)
+    }
+    res.render("configuration/outcome.ejs",{
+        message,result
+    })
+
+  }else{
+    res.redirect("/configuration/category")
+  }
+})
+//RESERVATION ROUTES
+router.get("/reservationList",(req,res)=>{
+  res.render("configuration/reservationList")
+});
+router.get("/pending",(req,res)=>{
+  res.render("configuration/pending",{reservationsToApprove})
+});
+router.post("/handle",(req,res)=>{
+  res.send("Done!")
+});
+
+router.get("/approvalHistory",(req,res)=>{
+  res.render("configuration/approvalHistory",{reservationHistory})
+})
+
+//helper functions
+
+async function getAllAdmins(){
+    let adminNameList=[]
+    try {
+      const rawList = await userModel.getAdminUsers()
+      rawList.forEach(a => {
+        adminNameList.push(`${a.FirstName} ${a.LastName}`)
+      });
+    } catch (error) {
+      console.error("Error retrieving Admins from Database",error)
+    }
+    return adminNameList;
+
+}
+async function getAllCategories(){
+    let categoryList;
+    try {
+    categoryList = await categoryModel.retrieveAll();
+    console.log(categoryList);
+    
+  } catch (error) {
+    console.error("Error retrieving Event Categories",error)
+  }
+  return categoryList
+}
+function validateCategoryEntry(catDesc,catName,approver,catList){
+  let isDuplicateCategory = false;
+  let isMissingDescription = !catDesc;
+  let isMissingCategoryName = !catName;
+  let isInvalidApprover = (approver==="");
+
+  let errorList =[];
+  if (isMissingCategoryName){
+    errorList.push("Missing Category Name");
+  }
+  if (isMissingDescription){
+    errorList.push("Missing a Category description");
+  }
+  
+  if(isInvalidApprover){
+    errorList.push("Missing a Valid Approver: Please select 'ANY ADMIN' or one admin user");
+  }
+
+  
+ 
+  
+
+  if (!isMissingCategoryName){
+      catList.forEach(c => {
+      const newCat = catName.trim().toLowerCase();
+      const currentCat = String(c.CategoryName).trim().toLowerCase();
+      // similarity and duplicate entry checks
+      if ( newCat === currentCat ){
+          isDuplicateCategory=true;
+          
+          errorList.push("Category Name exists")
+  
+        }
+        
+      else if(newCat.includes(currentCat) || currentCat.includes(newCat)){
+          errorList.push("Category Name is similar to an existing category.")
+          isDuplicateCategory=true;
+      }  
+      });
+  }
+  let validEntry = !isDuplicateCategory && !isMissingCategoryName && !isMissingDescription && !isInvalidApprover;
+
+  return {validEntry,errorList,approverId:approver}
+}
+
+async function getCategoryById(id){
+  let categoryDetail = {}
+  try {
+    let data = await categoryModel.retrieveAll();
+    categoryDetail = data.filter(category => category.CategoryID === id)[0]
+  } catch (error) {
+    console.error("Error retrieving Category Data",error)
+  }
+  return categoryDetail;
+
+}
