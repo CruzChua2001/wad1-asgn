1ae20c41f3be0d961f02139197a05e0f0fcbc480
mahshukurra2025 <264659135+mahshukCodes@users.noreply.github.com>
2026-03-26 21:32:33 +0800

    update reservation approval pt1

--- models/reservationModel.js
+++ models/reservationModel.js
@@ -141,7 +141,9 @@ exports.retrieveByUser = (userId) => {
 exports.retrievePending = () => {
     return Reservation.find({Status:"pending", isDeleted: 0});
 }
-
+exports.retrieveApprovedByAdminId = (adminId) => {
+    return Reservation.find({Status:"approved","ApprovedBy":adminId})
+}
 exports.create = (reservationData) => {
     const reservation = new Reservation(reservationData);
     return reservation.save();
